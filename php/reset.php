15cf7b0f0d4151bd6ea9e0c62ccd4376ca00390c
brucean52 <bruce.an52@gmail.com>
2018-03-06 15:25:59 -0800

    created reset.php for cron job

---
+++ php/reset.php
@@ -0,0 +1,41 @@
+<?php
+    require_once('../config/mysql_connect.php');
+    $output = [
+        'success' => false,
+        'error' => [],
+        'id' => null
+    ];
+    
+    $drop = "DROP TABLE `list`";
+    $result = mysqli_query($conn, $drop);
+
+    $create = "CREATE TABLE `list` (
+      `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+      `firstName` varchar(25) NOT NULL,
+      `lastName` varchar(25) NOT NULL,
+      `email` varchar(100) NOT NULL,
+      `phone` varchar(20) NOT NULL
+        )";
+
+    if (mysqli_query($conn, $create)) {
+        echo "Table list created successfully";
+    } else {
+        echo "Error creating table: " . mysqli_error($conn);
+    }
+
+    $query = "INSERT INTO `list` (`id`, `firstName`, `lastName`, `email`, `phone`) VALUES
+    (1, 'John', 'Wayne', 'test@gmail.com', '(243) 234-3920'),
+    (2, 'Britney', 'Spears', 'winwin@gmail.com', '(583) 098-0980'),
+    (3, 'Justine', 'Chen', 'moocow@hotmail.com', '(342) 083-2094'),
+    (4, 'William', 'Wong', 'haha@mail.com', '(329) 484-0930'),
+    (5, 'Angela', 'Smith', 'harhar@hotmail.com', '(230) 498-9800'),
+    (6, 'Jeff', 'Greene', 'poor@gmail.com', '(932) 048-0880'),
+    (7, 'Harry', 'Allen', 'rightnow@aol.com', '(043) 298-0943'),
+    (7, 'Whitney', 'Houston', 'singer@gmail.com', '(543) 438-0309');
+    ";
+
+    $result = mysqli_query($conn, $query);
+    print($result);
+
+    mysqli_close($conn);
+?>
