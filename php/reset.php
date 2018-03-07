0f66273a01fa211ef2d857e432acecaf81ab7c21
brucean52 <bruce.an52@gmail.com>
2018-03-06 20:23:59 -0800

    removed id from reset.php

--- php/reset.php
+++ php/reset.php
@@ -20,16 +20,15 @@
 //        echo "Error creating table: " . mysqli_error($conn);
 //    }
 
-    $query = "INSERT INTO `list` (`id`, `firstName`, `lastName`, `email`, `phone`) VALUES
-    (1, 'John', 'Wayne', 'test@gmail.com', '(243) 234-3920'),
-    (2, 'Britney', 'Spears', 'winwin@gmail.com', '(583) 098-0980'),
-    (3, 'Justine', 'Chen', 'moocow@hotmail.com', '(342) 083-2094'),
-    (4, 'William', 'Wong', 'haha@mail.com', '(329) 484-0930'),
-    (5, 'Angela', 'Smith', 'harhar@hotmail.com', '(230) 498-9800'),
-    (6, 'Jeff', 'Greene', 'poor@gmail.com', '(932) 048-0880'),
-    (7, 'Harry', 'Allen', 'rightnow@aol.com', '(043) 298-0943'),
-    (7, 'Whitney', 'Houston', 'singer@gmail.com', '(543) 438-0309');
-    ";
+    $query = "INSERT INTO `list` (`firstName`, `lastName`, `email`, `phone`) VALUES
+    ('John', 'Wayne', 'test@gmail.com', '(243) 234-3920'),
+    ('Britney', 'Spears', 'winwin@gmail.com', '(583) 098-0980'),
+    ('Justine', 'Chen', 'moocow@hotmail.com', '(342) 083-2094'),
+    ('William', 'Wong', 'haha@mail.com', '(329) 484-0930'),
+    ('Angela', 'Smith', 'harhar@hotmail.com', '(230) 498-9800'),
+    ('Jeff', 'Greene', 'poor@gmail.com', '(932) 048-0880'),
+    ('Harry', 'Allen', 'rightnow@aol.com', '(043) 298-0943'),
+    ('Whitney', 'Houston', 'singer@gmail.com', '(543) 438-0309');";
 
     $query_result = mysqli_query($conn, $query);
     print($result);
